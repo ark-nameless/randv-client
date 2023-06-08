c0b976885d28f4b8259bd9ce44277def32edd5c2
Ark Nameless <ark.nameless.zero@gmail.com>
2023-06-08 21:41:29 +0800

    added changes to ui on amnities.

--- src/app/views/main/extras/amneties/amneties.component.ts
+++ src/app/views/main/extras/amneties/amneties.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { Galleria } from 'primeng/galleria';
 import { PhotoService } from 'src/app/services/photo-service.service';
 
 @Component({
   selector: 'app-amneties',
   templateUrl: './amneties.component.html',
-  styleUrls: ['./amneties.component.css']
+  styleUrls: ['./amneties.component.scss']
 })
 export class AmnetiesComponent {
   images: any[] = [];
@@ -24,9 +25,101 @@ export class AmnetiesComponent {
     }
   ];
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private cd: ChangeDetectorRef,
+  ) {
+
+  }
 
   ngOnInit() {
     this.images = this.photoService.getImages();
+    this.bindDocumentListeners();
+  }
+
+  showThumbnails!: boolean;
+
+  fullscreen: boolean = false;
+
+  activeIndex: number = 0;
+
+  onFullScreenListener: any;
+
+  @ViewChild('galleria') galleria!: Galleria;
+
+  onThumbnailButtonClick() {
+    this.showThumbnails = !this.showThumbnails;
+  }
+
+  toggleFullScreen() {
+    if (this.fullscreen) {
+      this.closePreviewFullScreen();
+    } else {
+      this.openPreviewFullScreen();
+    }
+
+    this.cd.detach();
+  }
+
+  openPreviewFullScreen() {
+    let elem = this.galleria.element.nativeElement.querySelector('.p-galleria');
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem['mozRequestFullScreen']) {
+      /* Firefox */
+      elem['mozRequestFullScreen']();
+    } else if (elem['webkitRequestFullscreen']) {
+      /* Chrome, Safari & Opera */
+      elem['webkitRequestFullscreen']();
+    } else if (elem['msRequestFullscreen']) {
+      /* IE/Edge */
+      elem['msRequestFullscreen']();
+    }
+  }
+
+  onFullScreenChange() {
+    this.fullscreen = !this.fullscreen;
+    this.cd.detectChanges();
+    this.cd.reattach();
+  }
+
+  closePreviewFullScreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if ((<any>document)['mozCancelFullScreen']) {
+      (<any>document)['mozCancelFullScreen']();
+    } else if ((<any>document)['webkitExitFullscreen']) {
+      (<any>document)['webkitExitFullscreen']();
+    } else if ((<any>document)['msExitFullscreen']) {
+      (<any>document)['msExitFullscreen']();
+    }
+  }
+
+  bindDocumentListeners() {
+    this.onFullScreenListener = this.onFullScreenChange.bind(this);
+    document.addEventListener('fullscreenchange', this.onFullScreenListener);
+    document.addEventListener('mozfullscreenchange', this.onFullScreenListener);
+    document.addEventListener('webkitfullscreenchange', this.onFullScreenListener);
+    document.addEventListener('msfullscreenchange', this.onFullScreenListener);
+  }
+
+  unbindDocumentListeners() {
+    document.removeEventListener('fullscreenchange', this.onFullScreenListener);
+    document.removeEventListener('mozfullscreenchange', this.onFullScreenListener);
+    document.removeEventListener('webkitfullscreenchange', this.onFullScreenListener);
+    document.removeEventListener('msfullscreenchange', this.onFullScreenListener);
+    this.onFullScreenListener = null;
+  }
+
+  ngOnDestroy() {
+    this.unbindDocumentListeners();
+  }
+
+  galleriaClass() {
+    return `custom-galleria ${this.fullscreen ? 'fullscreen' : ''}`;
+  }
+
+  fullScreenIcon() {
+    return `pi ${this.fullscreen ? 'pi-window-minimize' : 'pi-window-maximize'}`;
   }
 }
