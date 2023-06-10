35e19ddd58da88b01796dcd4646602ae6fa0c6f1
Ark Nameless <ark.nameless.zero@gmail.com>
2023-06-10 12:09:28 +0800

    fixed logout button.

--- src/app/views/admin/index/index.component.ts
+++ src/app/views/admin/index/index.component.ts
@@ -7,7 +7,7 @@ import { SessionService } from 'src/app/services/session.service';
 @Component({
   selector: 'app-admin',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss'],
+  styleUrls: ['./index.component.css'],
   providers: [MessageService]
 })
 export class IndexComponent {
@@ -19,77 +19,23 @@ export class IndexComponent {
     private authService: AuthService,
     private sessionService: SessionService,
     private messageService: MessageService,
-  ) { }
+  ) 
+  { }
 
   ngOnInit() {
     this.items = [
       {
-        label: 'Dashboard',
-        icon: 'pi pi-fw pi-home',
-        routerLink: 'dashboard'
-      },
-      {
-        label: 'Reservations',
-        icon: 'pi pi-fw pi-calendar',
+        label: 'Options',
         items: [
           {
-            label: 'View All',
-            icon: 'pi pi-fw pi-search',
-            routerLink: 'reservations/'
-          },
-          {
-            label: 'Cancel Requests',
-            icon: 'pi pi-fw pi-user-minus',
-            routerLink: 'reservations/cancel'
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            command: () => {
+              this.logout();
+            }
           },
         ]
       },
-      {
-        label: 'Packages',
-        icon: 'pi pi-fw pi-file-edit',
-        items: [
-          {
-            label: 'View All',
-            icon: 'pi pi-fw pi-search',
-            routerLink: 'packages/all'
-          },
-          {
-            label: 'New',
-            icon: 'pi pi-fw pi-user-minus',
-            routerLink: 'packages/new'
-          },
-        ]
-      },
-      {
-        label: 'Events',
-        icon: 'pi pi-fw pi-calendar',
-        items: [
-          {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-              {
-                label: 'Save',
-                icon: 'pi pi-fw pi-calendar-plus'
-              },
-              {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-calendar-minus'
-              }
-            ]
-          },
-          {
-            label: 'Archieve',
-            icon: 'pi pi-fw pi-calendar-times',
-            items: [
-              {
-                label: 'Remove',
-                icon: 'pi pi-fw pi-calendar-minus'
-              }
-            ]
-          }
-        ]
-      }
     ];
 
     this.username = this.sessionService.getUser().username;
